11221f223a497521ec55fc26dd6064c55bb2cd49
Prahadeesh K <60143336+prahadeesh189@users.noreply.github.com>
2023-03-23 15:01:55 +0530

    ui changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -15,32 +15,32 @@ function Welcome() {
     return (
         <div className="Welcome">
 
-            
-            <h1>Welcome</h1>
+            <div className="welcome-wrapper">
+                <h1>Welcome</h1>
 
-
-
-            <div className="intro">
-                We at Information Science and Technology Association, are a staff-student run body striving to work hard for the betterment of The Department of Information Science and Technology, College of Engineering Guindy.
-            </div>
-
-            <div className="social-div">
-                <div className='social-handles'>
-                    <a href="https://www.instagram.com/ista__ceg/" target="_blank">
-                        <img src="../Instagram-icon.svg" alt="" />
-                    </a>
-                    <a href="mailto:auista.council@gmail.com" target="_blank">
-                        <img src="../email-icon.svg" class="email-logo" alt="" />
-                    </a>
-                    <a href="https://www.linkedin.com/company/ista-ceg/mycompany/" target="_blank">
-                        <img src="../linkedin-icon.svg" alt="" />
-                    </a>
+                <div className="intro">
+                    We at Information Science and Technology Association, are a staff-student run body striving to work hard for the betterment of The Department of Information Science and Technology, College of Engineering Guindy.
                 </div>
-            </div>
 
-            <button className="explore">
-                Explore
-            </button>
+                <div className="social-div">
+                    <div className='social-handles'>
+                        <a href="https://www.instagram.com/ista__ceg/" target="_blank">
+                            <img src="../Instagram-icon.svg" alt="" />
+                        </a>
+                        <a href="mailto:auista.council@gmail.com" target="_blank">
+                            <img src="../email-icon.svg" class="email-logo" alt="" />
+                        </a>
+                        <a href="https://www.linkedin.com/company/ista-ceg/mycompany/" target="_blank">
+                            <img src="../linkedin-icon.svg" alt="" />
+                        </a>
+                    </div>
+                </div>
+
+                <button className="explore">
+                    Explore
+                </button>
+            </div>
+            
 
             <div className="bg">
                 <div className="bg-end" />
