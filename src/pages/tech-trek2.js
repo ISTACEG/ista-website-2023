9d131d0ad2703434e8115a9054850b5bde0c7047
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-09-15 15:52:08 +0530

    appbar in techtrek

--- src/pages/tech-trek2.js
+++ src/pages/tech-trek2.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import "./tech-trek2.scss";
+import Navbar from "../components/navbar";
 
 function Techtrek2() {
     useEffect(() => {
@@ -7,7 +8,12 @@ function Techtrek2() {
         console.log("techtrek2");
       }, []);
     
-  return <p className="text1">Table here</p>;
+  return(
+    <>
+    <Navbar/>
+    <p className="text1">Table here</p>
+  </>
+)
 }
 
 export default Techtrek2;
