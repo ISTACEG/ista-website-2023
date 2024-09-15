f263dd1cb3f5b5a21ff0fc73a0ab89c6d3de1edd
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-09-15 18:01:21 +0530

    .

--- src/pages/tech-trek2.js
+++ src/pages/tech-trek2.js
@@ -6,13 +6,13 @@ import axios from "axios";
 function Techtrek2() {
   const [data, setData] = useState([]);
   
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    console.log("techtrek2");
 
-    const API_KEY = process.env.REACT_APP_GOOGLE_SHEETS_API_KEY;
-    const spreadsheetId = process.env.REACT_APP_SHEET_ID;
-    const range = "Sheet1!A1:H50";
+  useEffect(() => {
+  console.log("techtrek2");
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const spreadsheetId = process.env.REACT_APP_SHEET_ID;
+  console.log(API_KEY);
+  const range = "Sheet1!A1:H50";
     axios
       .get(
         `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${API_KEY}`
@@ -26,26 +26,25 @@ function Techtrek2() {
       });
   }, []);
 
-  const header= ["Name" , "Week1","Total"];
+  const header = ["Name", "Week1", "Total"];
 
   return (
     <div className="tech-trek2">
       <Navbar />
-      
+      <h1 className="title1">Tech Trek 2.0 Leaderboard</h1>
       <table className="table">
         <thead>
           <tr>
             {data.length > 0 &&
-              header.map((header, index) => (
-                <th key={index}>{header}</th>
-              ))}
+              header.map((header, index) => <th key={index}>{header}</th>)}
           </tr>
         </thead>
         <tbody>
           {data.slice(3).map((row, rowIndex) => (
             <tr key={rowIndex}>
-                <td>{row[7]}</td>
-                <td>{row[7]}</td>
+              <td>{row[0]}</td>
+              <td>{row[7]}</td>
+              <td>{row[7]}</td>
             </tr>
           ))}
         </tbody>
