17b60b99b84f2fa16cace9daa262d7cffad86c2e
YUVARAJ787 <yuvarajv787@gmail.com>
2024-09-23 19:04:24 +0530

    beta: experiences

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from "react";
 import styles from "../Experiences/Experiences.scss";
 import Navbar from "../navbar";
-import SideNavBar from "../SideNavBar/SideNavBar";
-import { ReactComponent as SearchSymbol } from "../../img/search.svg";
 import ExperienceBox from "./ExperienceBox";
 import axios from "axios";
-import companies from './../SideNavBar/companies';
 
 function Experiences() {
     const [data, setData] = useState([]);
     const [companies, setCompanies] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const rowsPerPage = 10;
+    const [searchQuery, setSearchQuery] = useState("");
+    const rowsPerPage = 12;
 
     function getDownloadLink(url) {
-
         const fileIdMatch = url.match(/id=([^&]+)/);
-        console.log(fileIdMatch)
         if (fileIdMatch && fileIdMatch[1]) {
             const fileId = fileIdMatch[1];
             return `https://drive.google.com/uc?export=download&id=${fileId}`;
@@ -34,19 +30,16 @@ function Experiences() {
                 `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${API_KEY}`
             )
             .then((response) => {
-                console.log(response.data.values); // this is array of array
-                // array[i] = [time, mail, name, phone, mail, company, view_link, ]
-                // append the download link to the array by using the getDownloadLink function
                 const updatedData = response.data.values.map(row => {
-                    const viewLink = row[6]; // assuming the view link is at index 6
+                    const viewLink = row[6];
+                    row[5] = row[5].toUpperCase().trim();
                     const downloadLink = getDownloadLink(viewLink);
                     return [...row, downloadLink];
                 });
                 setData(updatedData);
-                const uniqueCompanies = [...new Set(updatedData.map(row => row[5]))];
-                setCompanies(uniqueCompanies);                
-
                 console.log(updatedData)
+                const uniqueCompanies = [...new Set(updatedData.map(row => row[5]))];
+                setCompanies(uniqueCompanies);
             })
             .catch((error) => {
                 console.error("Error fetching data from Google Sheets", error);
@@ -55,9 +48,16 @@ function Experiences() {
 
     const indexOfLastRow = currentPage * rowsPerPage;
     const indexOfFirstRow = indexOfLastRow - rowsPerPage;
-    const currentRows = data.slice(indexOfFirstRow, indexOfLastRow);
+
+    const filteredData = data.filter(row => 
+        row[2].toLowerCase().includes(searchQuery.toLowerCase()) || // candidate name
+        row[5].toLowerCase().includes(searchQuery.toLowerCase()) // company name
+    );
+
+    const currentRows = filteredData.slice(indexOfFirstRow, indexOfLastRow);
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
 
     return (
         <div className="container">
@@ -65,35 +65,72 @@ function Experiences() {
             <div className="ExperienceBody">
                 <div className="inputBoxContainer">
                     <div className="form__group field">
-                        <input type="input" className="form__field" placeholder="Enter Company Name" required="" />
-                        <label htmlFor="name" className="form__label">Enter Company Name</label>
-                    </div>
-                    <div>
-                        <button style={{ border: "1px solid white", padding: "10px" }}>Search</button>
+                        <input 
+                            type="input" 
+                            className="form__field" 
+                            placeholder="Enter Company Name" 
+                            required="" 
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                        <label htmlFor="name" className="form__label">Search by company name / candidate name </label>
                     </div>
                 </div>
+                <div className="filterBar" style={{ overflowX: 'scroll', background:"transparent", whiteSpace: 'nowrap', scrollbarColor: 'darkblue lightgray', scrollbarWidth: 'thin'}}>
+                    <style>
+                        {`
+                            .filterBar::-webkit-scrollbar {
+                                height: 8px;
+                            }
+                            .filterBar::-webkit-scrollbar-track {
+                                background: lightgray;
+                            }
+                            .filterBar::-webkit-scrollbar-thumb {
+                                background-color: darkblue;
+                                border-radius: 10px;
+                            }
+                        `}
+                    </style>
+                    {companies.sort().map((company, index) => (
+                        <button 
+                            key={index} 
+                            onClick={() => setSearchQuery(company)}
+                            style={{ 
+                                padding: '10px', 
+                                backgroundColor: searchQuery === company && 'white', 
+                                color: searchQuery === company && 'black',
+                                display: 'inline-block',
+                                margin:"10px",
+                                borderRadius: "10px"
+                            }}
+                        >
+                            {company}
+                        </button>
+                    ))}
+                </div>
                 <div className="ExperienceDetails">
-                    <div>
-                        <SideNavBar names={companies} />
-                    </div>
                     <div className="EBContainer">
-                        <div className="EBFilters">
-                            <div><button style={{ border: "1px solid white", padding: "10px" }}>All</button></div>
-                            <div><button style={{ border: "1px solid white", padding: "10px" }}>Placements</button></div>
-                            <div><button style={{ border: "1px solid white", padding: "10px" }}>Internship</button></div>
-                        </div>
-                        <div>
+                        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                             {currentRows.map((row, index) => (
-                                <ExperienceBox key={index} data={row} />
+                                <div key={index} style={{ flex: '1 0 21%', margin: '10px' }}>
+                                    <ExperienceBox data={row} />
+                                </div>
                             ))}
                         </div>
-                        <div className="pagination">
-                            {Array.from({ length: Math.ceil(data.length / rowsPerPage) }, (_, i) => (
-                                <button key={i} onClick={() => paginate(i + 1)}>
+                        { Math.ceil(filteredData.length / rowsPerPage) != 1 && <div className="pagination">
+                            {Array.from({ length: Math.ceil(filteredData.length / rowsPerPage) }, (_, i) => (
+                                <button 
+                                    key={i} 
+                                    onClick={() => paginate(i + 1)}
+                                    style={{ 
+                                        backgroundColor: currentPage === i + 1 ? 'blue' : 'white', 
+                                        color: currentPage === i + 1 ? 'white' : 'black' 
+                                    }}
+                                >
                                     {i + 1}
                                 </button>
                             ))}
-                        </div>
+                        </div>}
                     </div>
                 </div>
             </div>
--- src/components/SideNavBar/SideNavBar.js
+++ src/components/SideNavBar/SideNavBar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../SideNavBar/SideNavBar.scss";
 
-function SideNavBar({names}) {
+function SideNavBar({ names }) {
     return (
-        <div className="SideNavBarContainer">
-            <h2>Companies</h2>
-            <ul>
-                {names.map((company, index) => (
-                    <li key={index}>{company}</li>
-                ))}
-            </ul>
+        <div className="SideNavBarContainer" style={{ width:"500px" }}>
+            <div className="scrollableContainer">
+                <ul>
+                    {names.map((company, index) => (
+                        <li key={index}>{company}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
